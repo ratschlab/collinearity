4fdc2adeaeb56658c9bfbb4c6650c9e3c45543ab
sayan <sayan.nitd@gmail.com>
2024-12-09 11:48:09 +0100

    more comprehensive tests for unique counts

--- tests/test_cqutils.cpp
+++ tests/test_cqutils.cpp
@@ -30,12 +30,29 @@ void test_cqutils() {
 
     check(parlay::equal(keys, res_keys), "Keys match");
     check(parlay::equal(values, res_values), "Values match");
+    check(parlay::is_sorted(res_keys), "Result keys are sorted");
 
     cqkeys.push_back(res_keys.data(), N);
     CQueue<u4> unique_keys(1 MiB, true);
-    CQueue<u4> counts;
+    CQueue<u4> counts(1 MiB, false);
     cq_count_unique(cqkeys, 1 MiB, buf, unique_keys, counts);
-    info("Found %zd unique keys", unique_keys.size());
+    size_t Nu = unique_keys.size();
+    info("Found %zd unique keys", Nu);
+    check(counts.size() == Nu, "Each key has a count");
+    parlay::sequence<u4> unique_keys_sorted(Nu), unique_key_counts(Nu);
+    unique_keys.pop_front(unique_keys_sorted.data(), Nu);
+    counts.pop_front(unique_key_counts.data(), Nu);
+    check(parlay::is_sorted(unique_keys_sorted), "Unique keys are sorted");
+
+    auto histogram = parlay::histogram_by_key(keys);
+    parlay::sort_inplace(histogram);
+    check(histogram.size() == Nu, "Correct count of keys");
+
+    auto same = parlay::tabulate(Nu, [&](size_t i){
+        return histogram[i].first == unique_keys_sorted[i] && histogram[i].second == unique_key_counts[i];
+    });
+    check(parlay::all_of((same), [&](auto &v){return v;}), "Correct keys and counts");
+
     unique_keys.clear();
     counts.clear();
 
--- tests/testmain.cpp
+++ tests/testmain.cpp
@@ -3,9 +3,25 @@
 //
 
 #include "tests.h"
+#include "parlay/parallel.h"
+#include "parlay/primitives.h"
+#include "parlay/sequence.h"
 
 int main(int argc, char *argv[]) {
-    test_compressed_array();
-    test_cqueue();
+//    test_compressed_array();
+//    test_cqueue();
     test_cqutils();
+
+    // Input: a sorted sequence
+    parlay::sequence<int> sorted_seq = {0,1,1,2,2,2,2,3,3,3,4,4,4,5,6,6,7,8,8,8,9};
+
+    auto kv = parlay::map(sorted_seq, [&](size_t i){
+        return std::make_pair(sorted_seq[i], 1);
+    });
+    auto rle = parlay::group_by_key_ordered(kv);
+
+    auto histogram = parlay::histogram_by_key(sorted_seq);
+    parlay::sort_inplace(histogram);
+    for (auto &[c, n] : histogram)
+        printf("%d - %zd\n", c, n);
 }
