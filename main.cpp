95cfcc007fe7aa7a38a787a576e13f417e6a0c9a
sayan <sayan.nitd@gmail.com>
2024-12-11 10:07:27 +0100

    works but is very slow

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
     const char *ref = "/scratch/Zymo/Refs-fwd-rev.fasta";
-    const char *qry = "/scratch/Zymo/reads-tiny.fasta";
+    const char *qry = "/scratch/Zymo/basecalled/fast/Sigs.fasta"; //"/scratch/Zymo/reads-tiny.fasta";
     auto [idx, refnames] = process_fasta(ref, 10, 4);
     query(qry, 10, 4, 512, idx, refnames);
     return 0;
